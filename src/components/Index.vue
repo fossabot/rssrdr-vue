b1493c5ae672089d398212d97064fda5cf2e40a0
Chetan <chetan.narian@gmail.com>
2017-08-06 12:26:07 +0530

    Loading the data for selected feed

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,35 +1,87 @@
 <template>
-  <div class="hello">
+  <div class="container">
     <h1>{{ title }}</h1>
 
     <input type="text" placeholder="Enter feed url" v-model="feed_url">
     <button type="button" v-on:click="sync" name="button">Sync!</button>
     <br><br>
-    <router-link to="/myfeed">My Feed</router-link>
+    <div class="feed" v-for="feed in urls">
+      <input type="radio" v-on:click="selectFeed" v-model="selected" name="feed" :value="feed.name"> {{ feed.name }}
+    </div>
+    <div class="feed-container">
+      <h3>{{selectedFeed.title}}</h3>
+      <ol class="selected-feed">
+        <li v-for="entry in selectedFeed.entries">
+          <a target="_blank" :href="entry.link">{{entry.title}}</a>
+          <div>{{entry.content}}</div>
+        </li>
+      </ol>
+    </div>
+    <!-- <router-link to="/myfeed">My Feed</router-link> -->
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import _ from 'lodash';
+
 
 export default {
   name: 'index',
   data () {
     return {
       title: 'RSSRDR',
-      feed_url: ''
+      urls: [],
+      feed_url: '',
+      selected: '',
+      selectedFeed: {}
     }
   },
+  created () {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.fetchData()
+    // this.fetchFeed('https://nodesource.com/blog/rss')
+  },
   methods: {
-    sync: function () {
-      axios.post('http://express.rssrdr.net:4000/saveFeed/?url='+this.feed_url)
+    fetchData () {
+      axios.get('http://express.rssrdr.net:4000/getSavedLinks/?id=5983c64cfee97317acef37ef')
       .then(response => {
-        console.log(response.data);
+        if(response && response.data) {
+          this.urls = response.data.urls;
+        }
+      })
+      .catch(e => {
+        console.error(e);
+      });
+    },
+    sync () {
+      axios.post('http://express.rssrdr.net:4000/saveRSSLink/?url='+this.feed_url)
+      .then(response => {
         this.feed_url = '';
       })
       .catch(e => {
         console.error(e);
       });
+    },
+    fetchFeed (url) {
+      axios.get('http://express.rssrdr.net:4000/getFeedData/?url='+url)
+      .then(response => {
+        if(response && response.data) {
+          this.selectedFeed = response.data;
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      });
+    },
+    selectFeed () {
+      let slctd = '',
+          u_slctd = this.selected;
+      if(u_slctd) {
+        slctd = _.find(this.urls, function(url){ return url.name == u_slctd});
+        this.fetchFeed(slctd.url);
+      }
     }
   }
 }
@@ -47,11 +99,18 @@ ul {
 }
 
 li {
-  display: inline-block;
   margin: 0 10px;
+}
+
+.selected-feed li {
+  text-align: left;
 }
 
 a {
   color: #42b983;
+}
+
+.feed {
+  text-align: center;
 }
 </style>
